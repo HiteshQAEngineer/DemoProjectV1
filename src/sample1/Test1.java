5bb931580e8512f339b7038cba388c2301ab562d
hites <hites@Dynamo>
2025-02-21 12:47:56 +0530

    First Commit....

---
+++ src/sample1/Test1.java
@@ -0,0 +1,19 @@
+package sample1;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		String name = "Git";
+		
+		if (name.equals("Git")) {
+			System.out.println("Working on the Git");
+		}
+		else {
+			System.out.println("Not working on the Git");
+		}
+			
+		
+	}
+
+}
